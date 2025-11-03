01c909ae3246e14a382af4c73c9eea8487852c21
ALLYX\admin <abc@NOADDRESS.com>
2025-11-03 06:29:49 -0400

    no message

--- core/klippy/extras/print_stats.h
+++ core/klippy/extras/print_stats.h
@@ -1,71 +1,53 @@
-#ifndef WEB_H
-#define WEB_H
-#include "mongoose.h"
-
-#endif //WEB_H
-
-// TODO: get that from some config
-#define WEBSERVER_PORT 80
-#define WEBSERVER_SERVE_DIR "/app/resources/www,/network-device-manager/network/control=/app/resources/www,/thumbnail=/user-resource/file_info"
-#define WEBSOCKET_PORT 3030
-// Looking at various logs it seems to be the same for every CC, couldn't find how it is generated.
-#define SDCP_MACHINE_BRAND_IDENTIFIER "979d4C788A4a78bC777A870F1A02867A"
-#define SDCP_MAX_HANDLERS 20
-#define MAX_UPLOAD_FILENAME_LEN 255
-#define TEMP_PART_UPLOAD_FILE_PATH_FORMAT "/user-resource/.upload-part-%d.tmp"
-#define TEMP_UPLOAD_FILE_PATH_FORMAT "/user-resource/.upload-file-%s.tmp"
-
-typedef enum {
-    SDCP_CMD_REFRESH_STATUS = 0,
-    SDCP_CMD_ATTRIBUTES = 1
-} sdcp_cmd_t;
-
+#ifndef PRINT_STATS_H
+#define PRINT_STATS_H
+#include <string>
+#include "gcode.h"
+#include "print_stats_c.h"
 typedef enum
 {
-    SDCP_MACHINE_STATUS_IDLE = 0,  // Idle
-    SDCP_MACHINE_STATUS_PRINTING = 1,  // Executing print task
-    SDCP_MACHINE_STATUS_FILE_TRANSFERRING = 2,  // File transfer in progress
-    SDCP_MACHINE_STATUS_EXPOSURE_TESTING = 3,  // Exposure test in progress
-    SDCP_MACHINE_STATUS_DEVICES_TESTING = 4,  //Device self-check in progress
-} sdcp_machine_status_t;
-
-typedef enum
+    PRINT_STATS_INFO_UPDATE = 0,
+    PRINT_STATS_STATE_CHANGE = 1,
+} print_stats_event_t;
+class PrintStats
 {
-    SDCP_PRINT_ERROR_NONE = 0, // Normal
-    SDCP_PRINT_ERROR_CHECK = 1, // File MD5 Check Failed
-    SDCP_PRINT_ERROR_FILEIO = 2, // File Read Failed
-    SDCP_PRINT_ERROR_INVLAID_RESOLUTION = 3,  // Resolution Mismatch
-    SDCP_PRINT_ERROR_UNKNOWN_FORMAT = 4,  // Format Mismatch
-    SDCP_PRINT_ERROR_UNKNOWN_MODEL = 5,  // Machine Model Mismatch
-} sdcp_print_error_t;
-
-typedef void (*sdcp_event_handler)(struct mg_connection*, int, mg_ws_message*, void*);
-
-typedef struct {
-    int cmd;
-    sdcp_event_handler handler;
-} sdcp_handler_entry_t;
-
-typedef struct {
-    int offset;
-    int total_size;
-    bool check;
-    char s_file_md5[33];
-    char uuid[37];
-    char filename[MAX_UPLOAD_FILENAME_LEN+1];
-    char temp_file_path[64];
-} upload_info_t;
-
-typedef struct {
-    bool offset;
-    bool total_size;
-    bool check;
-    bool s_file_md5;
-    bool uuid;
-    bool file;
-} upload_info_fieldset_t;
-
-
-void webserver_start();
-void webserver_stop();
-void* webserver_task(void* arg);
+private:
+public:
+    PrintStats(std::string section_name);
+    ~PrintStats();
+    std::string m_filename;
+    const char *m_filename_without_path;
+    double m_last_epos; // 挤出机的位置，用来算出料长度
+    print_stats_t m_print_stats;
+    void _update_filament_usage(double eventtime);
+    void update_filament_usage(double eventtime);
+    void set_current_file(std::string filename, int src, std::string taskid);
+    double get_print_duration();
+    void note_start();
+    void note_actual_start();
+    void note_pause();
+    void note_pauseing();
+    void note_canceling();
+    void note_resuming();
+    void note_complete();
+    void note_error();
+    void note_cancel();
+    void note_change_filament_start();
+    void note_change_filament_completed();
+    void _note_finish(print_stats_state_s state);
+    void reset();
+    print_stats_t get_status(double eventtime, print_stats_t *cur_state);
+    void cmd_M117(GCodeCommand &gcode);
+    void cmd_SET_PRINT_STATS_INFO(GCodeCommand &gcode);
+    void set_task_id(std::string taskid);
+    std::string get_task_id(void);
+    void set_slice_param(slice_param_t slice_param);
+    void set_total_layers(uint32_t total_layers);
+};
+typedef void (*print_stats_state_callback_t)(int event);
+int print_stats_register_state_callback(print_stats_state_callback_t state_callback);
+int print_stats_unregister_state_callback(print_stats_state_callback_t state_callback);
+int print_stats_state_callback_call(int event);
+print_stats_state_s print_stats_get_state(void);
+int print_stats_set_state(print_stats_state_s status);
+int get_slice_param(slice_param_t *slice_param);
+#endif
